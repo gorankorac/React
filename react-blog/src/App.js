206b2915489e84ed3737a30c732a2b6892cada4a
gorankorac <gorankorac@gmail.com>
2023-03-04 15:07:55 +0100

    react task

---
+++ react-blog/src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+
+import { Header } from './components/Header/Header';
+import {Main} from './components/maincontent/Maincontent'
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+      <Main></Main>
+
+
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ react-blog/src/components/maincontent/Maincontent.js
@@ -0,0 +1,35 @@
+import './Maincontent.css'
+
+export const Main = () => {
+  return (
+    <div className='container'>
+    
+    <div className='item'>
+      <h3>
+         First Blog
+      </h3>
+      <p>lorem ipsum </p>
+    </div>
+    
+    <div className='item'>
+       <h3>
+          Second Blog
+       </h3>
+         <p>lorem ipsum lkdlvnln</p>
+    </div>
+    <div className='item'>
+       <h3>
+          Third Blog
+       </h3>
+         <p>ldjlndlbnldnbldnbldnbldnbldnbldnl</p>
+    </div>
+    <div className='item'>
+       <h3>
+          Fourth Blog
+       </h3>
+         <p>ldjlndlbnldnbldnbldnbldnbldnbldnl</p>
+    </div>
+    
+    </div>
+  )
+}
